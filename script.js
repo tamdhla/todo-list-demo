0d7d390f4ef0a19bf1bf5038a72848ac4646754c
Nguyen Huu Tam <tamblla1984@gmail.com>
2025-04-10 08:05:08 +0700

    Initial commit: Setup basic HTML, CSS, JS structure

---
+++ script.js
@@ -0,0 +1,10 @@
+function addTask() {
+    const input = document.getElementById('taskInput');
+    const task = input.value;
+    if (task) {
+        const li = document.createElement('li');
+        li.textContent = task;
+        document.getElementById('taskList').appendChild(li);
+        input.value = '';
+    }
+}
